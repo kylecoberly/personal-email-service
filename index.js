d69a2060e010f24eb1cf755db8e4a4e20ed9c389
Kyle Coberly <kyle.coberly@gmail.com>
2020-04-08 18:28:36 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const sgMail = require('@sendgrid/mail');
+
+const app = express()
+app.use(bodyParser.json())
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+
+app.post("/email", (request, response) => {
+  const { email, message } = request.body
+
+  sgMail.send({
+    from: "kyle.coberly@gmail.com",
+    to: "kyle.coberly@gmail.com",
+    reply_to: email,
+    subject: "Inbound message from kylecoberly.com",
+    text: message,
+  }).then(() => {
+    response.sendStatus(200)
+  }).catch(error => {
+    console.error(error.message)
+    response.sendStatus(500)
+  });
+})
+
+app.listen(process.env.PORT, () => console.log("Listening"))
